c8f685aa929dce6bb371c9320852f859aa4aa9a0
shinta0000 <uenoshinta0k@gmail.com>
2020-08-23 07:37:54 +0900

    hooksを導入

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LikeButton from './LikeButton';
 
 const Article = (props) => {
+  const [isPublished, togglePublished] = useState(false);
+
   return (
     <div>
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態:</label>
-      <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()} />
+      <input type="checkbox" checked={isPublished} id="check" onClick={() => togglePublished(!isPublished)} />
       <LikeButton count={props.count} />
     </div>
   )
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -3,58 +3,38 @@ import Article from './Article';
 import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge';
 
-class Blog extends React .Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      count: 0
-    }
-  }
+const Blog = () => {
+  // componentDidMount() {
+  //   document.getElementById('counter').addEventListener('click', this.countUp)
+  // }
 
-  componentDidMount() {
-    document.getElementById('counter').addEventListener('click', this.countUp)
-  }
+  // componentDidUpdate() {
+  //   if (this.state.count >= 10) {
+  //     this.setState({ count: 0 })
+  //   }
+  // }
 
-  componentDidUpdate() {
-    if (this.state.count >= 10) {
-      this.setState({ count: 0 })
-    }
-  }
+  // componentWillUnmount() {
+  //   document.getElementById('counter').removeEventListener('click', this.countUp)
+  // }
 
-  componentWillUnmount() {
-    document.getElementById('counter').removeEventListener('click', this.countUp)
-  }
+  // countUp = () => {
+  //   this.setState({
+  //     count: this.state.count + 1
+  //   })
+  // }
 
-  // 公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    })
-  };
-
-  countUp = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
-  render() {
-    const authorName = 'Torahack'
-    return (
-      <>
-        <Article 
-          title={"Reactの使い方"} 
-          isPublished={this.state.isPublished} 
-          toggle={() => this.togglePublished()} 
-          count={this.state.count}
-        />
-        <FooBar.Foo />
-        <FooBar.Bar />
-        <Hoge />
-      </>
-    );
-  }
+  return (
+    <>
+      <Article 
+        title={"Reactの使い方"} 
+        // count={this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </>
+  );
 }
 
 export default Blog
