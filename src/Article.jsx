ffeba18f54cfca1764b25490acb259003f8efaa4
shinta0000 <uenoshinta0k@gmail.com>
2020-08-22 22:27:29 +0900

    added counter

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const Article = (props) => {
-  let publishState = '';
-  if (props.isPublished) {
-    publishState = '公開'
-  } else {
-    publishState = '非公開'
-  }
   return (
     <div>
       <h2>{props.title}</h2>
-      <p>順番は{props.order}です。</p>
+      <label htmlFor="check">公開状態:</label>
+      <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()} />
+      <p>{props.order}</p>
+      <button onClick={() => props.countup()}>+</button>
+      <button onClick={() => props.countdown()}>-</button>
     </div>
   )
 };
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,14 +4,36 @@ import Article from './Article';
 class Blog extends React .Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPublished: false,
+      order: 1
+    }
   }
+
+  // 公開状態を反転させる関数
+  togglePublished = () => {
+    this.setState({
+      isPublished: !this.state.isPublished
+    })
+  };
+
+  countUp = () => {
+    this.setState({
+      order: this.state.order + 1
+    })
+  };
+
+  countDown = () => {
+    this.setState({
+      order: this.state.order - 1
+    })
+  };
+
   render() {
     const authorName = 'Torahack'
     return (
       <>
-        <Article title={"Reactの使い方"} order={1} />
-        <Article title={"JSXの使い方"} order={2} />
-        <Article title={"Reduxの使い方"} order={3} />
+        <Article title={"Reactの使い方"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()} order={this.state.order} countup={() => this.countUp()} countdown={() => this.countDown()} />
       </>
     );
   }
